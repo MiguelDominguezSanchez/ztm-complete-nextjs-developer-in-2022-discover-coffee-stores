9a3d21cce883d341d692b269047221b5d371bb98
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-01-24 09:14:47 +0100

    lesson 29

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
+import styles from '../styles/Home.module.css'
 
 import Banner from '../components/banner'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
 	const handleOnBannerBtnClick = () => {
