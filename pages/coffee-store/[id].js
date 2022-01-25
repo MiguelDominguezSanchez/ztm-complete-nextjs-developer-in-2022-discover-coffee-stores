655a68cd4f0628a46f12a40aacf72a6f347ffefe
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2022-01-25 12:54:50 +0100

    lesson 66

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -3,9 +3,11 @@ import Link from 'next/link'
 import Head from 'next/head'
 import Image from 'next/image'
 
+import cls from 'classnames'
+
 import coffeeStoresData from '../../data/coffee-stores.json'
 
-import styles from '../../styles/coffee-store.  module.css'
+import styles from '../../styles/coffee-store.module.css'
 
 export function getStaticProps(staticProps) {
 	const params = staticProps.params
@@ -40,6 +42,10 @@ const CoffeeStore = (props) => {
 
 	const { address, name, neighbourhood, imgUrl } = props.coffeeStore
 
+	const handleUpvoteButton = () => {
+		console.log('handle upvote')
+	}
+
 	return (
 		<div className={styles.layout}>
 			<Head>
@@ -64,9 +70,23 @@ const CoffeeStore = (props) => {
 					></Image>
 				</div>
 
-				<div className={styles.col2}>
-					<p>{address}</p>
-					<p>{neighbourhood}</p>
+				<div className={cls('glass', styles.col2)}>
+					<div className={styles.iconWrapper}>
+						<Image src='/static/icons/places.svg' width='24' height='24' />
+						<p className={styles.text}>{address}</p>
+					</div>
+					<div className={styles.iconWrapper}>
+						<Image src='/static/icons/nearMe.svg' width='24' height='24' />
+						<p className={styles.text}>{neighbourhood}</p>
+					</div>
+					<div className={styles.iconWrapper}>
+						<Image src='/static/icons/star.svg' width='24' height='24' />
+						<p className={styles.text}>1</p>
+					</div>
+
+					<button className={styles.upvoteButton} onClick={handleUpvoteButton}>
+						Up vote!
+					</button>
 				</div>
 			</div>
 		</div>
